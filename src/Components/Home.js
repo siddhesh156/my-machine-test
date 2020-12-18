966d6b99d19f0b409ec1a85f5af0e132bc9f42fb
Siddhesh Chavan <siddhesh.aprilinnovations@gmail.com>
2020-12-18 17:16:54 +0530

    Initial Commit

---
+++ src/Components/Home.js
@@ -0,0 +1,26 @@
+import "./Home.css"
+
+const Home = () =>{
+    return(
+        <div className="home">
+            <h1>IG Account Details</h1>
+            <div className="d-f j-c">
+            <div className="oborder"></div>
+            </div> 
+
+            <div className="d-f subHeader">
+            <h2>Account Details</h2>
+            <div className="d-f a-c">
+            <button type="button" class="btn btn-outline-primary btn-lg">Contact List</button>
+            </div>
+           </div> 
+
+
+            <div className="d-f j-c">
+            <div className="dborder"></div>
+            </div>
+        
+        </div>
+    )
+}
+export default Home
